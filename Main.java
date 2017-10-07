1bfdbcdcd91a64aa97682ef69d2109fd9c264727
RomPavel <31888768+RomPavel@users.noreply.github.com>
2017-10-07 13:52:02 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,30 +1,58 @@
+/*
+ Павел Ромашевский, 2 курс 5 группа
+ Лабораторная работа №3
+ Вариант 8
+ "Из текста удалить все слова заданной длины, начинающиеся на согласную букву."
+  */
+
+
+
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static String deleteWords(String tochange, int l) {
+
+        final String delimiters = " .,!:;\"\n\t\r\f ";
+        final String consonants = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM";
+
+        StringBuilder answer = new StringBuilder();
+
+        StringTokenizer withdel = new StringTokenizer(tochange, delimiters, true);
+        while (withdel.hasMoreTokens()) {
+            String token = withdel.nextToken();
+
+            if ((consonants.indexOf(token.charAt(0))==-1)||token.length()!=l)
+                 answer.append(token);
+        }
+        return answer.toString();
+    }
     public static void main(String[] args) {
 
+        String[] text = new String[50];
+        int length = 0;
+        int count = 0;
+
         Scanner in = new Scanner(System.in);
-        int n=0;
-        int m=0;
 
-        System.out.print("Please, Enter the n & m: ");
+        System.out.println("Enter your text. Write 'exit' on new line to stop");
+        while (in.hasNextLine()) {
 
-        n=in.nextInt();
-        m=in.nextInt();
+            text[count] = new String(in.nextLine());
+            if(text[count].equals("exit")) break;
+            count++;
+        }
 
-        Matrix A = new Matrix(n, m);
+        in = new Scanner(System.in);
+        System.out.println("Enter word length.");
+        length = in.nextInt();
 
-        System.out.println("Please, Enter your Matrix");
-        A.Cin(in);
+        for (int i = 0 ; i < count; i++)
+            text[i]=deleteWords(text[i], length);
 
-       // A.Cout();
-
-        System.out.println("Max_Length_Egual_Sequence in row: "+A.rowIneed());
-
-        System.out.println("Col_Local_Minimum: "+A.local_min());
-
-        A.change_matrix();
-        A.Cout();
-
+        System.out.println("After deleting words:");
+        for (int i = 0; i < count; i++)
+            System.out.println(text[i]);
     }
 }
